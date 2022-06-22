120494b540dabc58232a2832429197fd7cfd077b
Victor F <fernandes.victor@hotmail.com>
2022-06-21 22:30:01 -0300

    Fixed input validation. Implemented convertion logic.

--- Bin2Dec/bin2dec.js
+++ Bin2Dec/bin2dec.js
@@ -1,19 +1,29 @@
 function convertButton() {
-  const binaryValue = document.getElementById("binary");
+  const binaryValue = document.getElementById("binary").value;
   if (validateInput(binaryValue)) {
     const decimalValueOutput = document.getElementById("decimal");
     const decimalValue = convertBinToDec(binaryValue);
     decimalValueOutput.innerText = decimalValue;
   } else {
     const decimalValueOutput = document.getElementById("decimal");
-    decimalValueOutput.innerText = 'Invalid Input! The string may be a 8-digit string containing only 1 and 0';
+    decimalValueOutput.innerText = 'Invalid Input! The string must be a 8-digit string containing only 1 and 0';
   }
 }
 
 function convertBinToDec(binaryValue) {
-  return "10";
+  let binaryValueArray = binaryValue.split("");
+  binaryValueArray = binaryValueArray.map(digit => parseInt(digit));
+  const decimalValue = binaryValueArray.reduce((acc, curr, index) => {
+    const expoent = 7 - index;
+    acc += Math.pow(2, expoent) * curr;
+    return acc;
+  });
+
+  return decimalValue;
 }
 
 function validateInput(binaryValue) {
-  return binaryValue.length === 8 && binaryValue.search(/[^0-1]/) !== -1;
+  const lengthIsEight = binaryValue.length === 8;
+  const hasNotBinValue = binaryValue.search(/[^0-1]/) === -1;
+  return lengthIsEight && hasNotBinValue;
 }
