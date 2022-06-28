19853edd67217c5268342fdb6b0a638a77706838
Victor F <fernandes.victor@hotmail.com>
2022-06-27 21:57:00 -0300

    Finished exercise

--- Bin2Dec/bin2dec.js
+++ Bin2Dec/bin2dec.js
@@ -6,7 +6,7 @@ function convertButton() {
     decimalValueOutput.innerText = decimalValue;
   } else {
     const decimalValueOutput = document.getElementById("decimal");
-    decimalValueOutput.innerText = 'Invalid Input! The string must be a 8-digit string containing only 1 and 0';
+    decimalValueOutput.innerText = 'Invalid Input! The string must contain only 1 and 0';
   }
 }
 
@@ -14,16 +14,16 @@ function convertBinToDec(binaryValue) {
   let binaryValueArray = binaryValue.split("");
   binaryValueArray = binaryValueArray.map(digit => parseInt(digit));
   const decimalValue = binaryValueArray.reduce((acc, curr, index) => {
-    const expoent = 7 - index;
+    const expoent = binaryValueArray.length - (index + 1);
     acc += Math.pow(2, expoent) * curr;
     return acc;
-  });
+  }, 0);
 
   return decimalValue;
 }
 
 function validateInput(binaryValue) {
-  const lengthIsEight = binaryValue.length === 8;
+  console.log(binaryValue.length === 0);
   const hasNotBinValue = binaryValue.search(/[^0-1]/) === -1;
-  return lengthIsEight && hasNotBinValue;
+  return hasNotBinValue && binaryValue.length !== 0;
 }
